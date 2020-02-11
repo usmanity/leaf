9744382a30b47f6ccefc3daf79ccccb1fb2d5eb5
Muhammad Usman <muhammad@fastmail.com>
2020-02-10 20:58:16 -0800

    improve font size on mobile and for leave a comment

--- header.php
+++ header.php
@@ -1,14 +1,12 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
-
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
   <link rel="profile" href="https://gmpg.org/xfn/11">
-  <link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro&display=swap" rel="stylesheet">
   <?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?>>
   <div id="page" class="site">
     <a class="skip-link screen-reader-text" href="#content">
