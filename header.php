407b395c377288885f8f98907d1a3d074bdd3ab7
Muhammad Usman <muhammad@fastmail.com>
2020-02-10 20:01:50 -0800

    fixing some typography issues

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="profile" href="https://gmpg.org/xfn/11">
   <link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro&display=swap" rel="stylesheet">
   <?php wp_head(); ?>
